cf5a2725cdbec969883e52284737299189171882
Kyle Mitchell <kyle@kemitchell.com>
2015-05-31 02:56:01 +0000

    Support adding entities to a party

--- js/index.js
+++ js/index.js
@@ -32,6 +32,7 @@ var dateStore = Reflux.createStore({
 
 var deleteBlock = Reflux.createAction();
 var addBlock = Reflux.createAction();
+var addEntity = Reflux.createAction();
 
 var blocksStore = Reflux.createStore({
   init: function() {
@@ -44,17 +45,24 @@ var blocksStore = Reflux.createStore({
       this.value = this.value.push(fromJS({person: 'John Doe'}));
       this.trigger(this.value);
     }.bind(this));
+    this.listenTo(addEntity, function(index) {
+      this.value = this.value.set(
+        index,
+        this.value.get(index).unshift(fromJS({
+          type: 'entity',
+          name: 'Some, Inc.',
+          role: ''
+        }))
+      );
+      this.trigger(this.value);
+    }.bind(this));
   },
   getInitialState: function() {
     return fromJS([
       [
         {
           type: 'person',
-          name: 'Errol Flynn'}],
-      [
-        {
-          type: 'person',
-          name: 'Basil Rathbone'}],
+          name: 'John Doe'}],
       [
         {
           type: 'entity',
@@ -66,16 +74,7 @@ var blocksStore = Reflux.createStore({
         {
           type: 'person',
           name: 'John Doe',
-          role: 'Chief Executive Officer'}],
-      [
-        {
-          type: 'entity',
-          name: 'Some Fund, LLP'},
-        {
-          type: 'entity',
-          name: 'Some Corporation, Inc.',
-          role: 'General Partner'},
-        {type: 'person'}]]);
+          role: ''}]]);
   }
 });
 
@@ -125,83 +124,107 @@ function render(name, renderFunction) {
   return component(name, {render: renderFunction});
 }
 
-var EntityLine = render('EntityLine', function() {
-  var entity = this.props.entity;
-  return p(
-    {className: 'line'},
-    [
-      entity.get('name'),
-      entity.has('role') ? ', its ' + entity.get('role') : null
-    ]
-  );
+var NameField = render('NameField', function() {
+  return div({className: 'wrapper'}, [
+    (this.props.name ?
+      span({className: 'printOnly'}, this.props.name) :
+      span({className: 'printOnly spacer'})),
+    React.DOM.input({placeholder: 'Name', value: this.props.name})
+  ]);
 });
 
-var BlankLine = render('BlankLine', function() {
-  return p(
-    {className: 'blank'},
-    [
-      span(
-        {className: 'label'},
-        this.props.label + ':'
-      ),
-      span({className: 'spacer'})
-    ]
-  );
+var RoleField = render('RoleField', function() {
+  return div({className: 'wrapper'}, [
+    (this.props.role ?
+      span({className: 'printOnly'}, this.props.role) :
+      span({className: 'printOnly spacer'})),
+    React.DOM.input({placeholder: 'Title', value: this.props.role})
+  ]);
+});
+
+var EntityLine = render('EntityLine', function() {
+  var entity = this.props.entity;
+  if (this.props.top) {
+    return div({className: 'line'}, [
+      create(NameField, {name: entity.get('name')})
+    ]);
+  } else {
+    return div({className: 'line'}, [
+      'By: ',
+      create(NameField, {name: entity.get('name')}),
+      ', its ',
+      create(RoleField, {role: entity.get('role')})
+    ]);
+  }
 });
 
 var FinalLine = render('FinalLine', function() {
   var person = this.props.person;
-  if (person.has('name')) {
-    return p(
-      {className: 'line simple final'},
-      [
-        person.get('name'),
-        person.has('role') ? ', ' + person.get('role') : null
-      ]
-    );
+  if (this.props.individual) {
+    return div({className: 'line simple final'}, [
+      create(NameField, {name: person.get('name')})
+    ]);
   } else {
-
-    return div(
-      null,
-      [
-        create(BlankLine, {label: 'By'}),
-        create(BlankLine, {label: 'Name'}),
-        create(BlankLine, {label: 'Title'})
-      ]
-    );
+    return div({className: 'final'}, [
+      div({className: 'blank'}, [
+        span({className: 'label'}, 'By: '),
+        span({className: 'spacer'})
+      ]),
+      div({className: 'blank'}, [
+        span({className: 'label'}, 'Name: '),
+        create(NameField, {name: person.get('name')})
+      ]),
+      div({className: 'blank'}, [
+        span({className: 'label'}, 'Title: '),
+        create(RoleField, {name: person.get('role')})
+      ])
+    ]);
   }
 });
 
 var Party = render('Party', function() {
-  return div(
-    null,
-    [
-      this.props.party
-        .slice(0, -1)
-        .map(function(entity, index) {
-          return create(EntityLine, {entity: entity});
-        }),
-      create(FinalLine, {person: this.props.party.last()})
-    ]
-  );
+  return div(null, [
+    this.props.party
+      .slice(0, -1)
+      .map(function(entity, index) {
+        return create(EntityLine, {
+          top: index === 0,
+          entity: entity
+        });
+      }),
+    create(FinalLine, {
+      individual: this.props.party.count() < 2,
+      person: this.props.party.last()
+    })
+  ]);
 });
 
 var DateLine = render('DateLine', function() {
-  return p(
-    {className: 'dateLine'},
+  return p({className: 'dateLine'}, [
     this.props.date === 'Each Signature' ? 'Date' : ''
-  );
+  ]);
+});
+
+var AddEntity = component('AddEntity', {
+  handleClick: function() {
+    addEntity(this.props.index);
+  },
+  render: function() {
+    return React.DOM.button({
+      onClick: this.handleClick
+    }, 'Add Entity');
+  }
 });
 
 var Block = render('Block', function() {
-  return div(
-    {className: 'block'},
-    [
-      this.props.date === 'Each Signature' ?
-        create(DateLine, {date: this.props.date}) : null,
-      create(Party, {party: this.props.party})
-    ]
-  );
+  return div({className: 'block'}, [
+    this.props.date === 'Each Signature' ?
+      create(DateLine, {date: this.props.date}) : null,
+    div({className: 'line editorOnly'}, [
+      create(AddEntity, {index: this.props.index})
+    ]),
+    create(Party, {party: this.props.party})
+  ]);
 });
 
 var TitleInput = component('TitleInput', {
@@ -215,18 +238,15 @@ var TitleInput = component('TitleInput', {
     this.setState({title: event.target.value});
   },
   render: function() {
-    return div(
-      null,
-      [
-        React.DOM.label(null, 'Agreement Title'),
-        React.DOM.input({
-          type: 'text',
-          onChange: this.handleChange,
-          onBlur: this.handleBlur,
-          value: this.state.title
-        })
-      ]
-    );
+    return div(null, [
+      React.DOM.label(null, 'Agreement Title'),
+      React.DOM.input({
+        type: 'text',
+        onChange: this.handleChange,
+        onBlur: this.handleBlur,
+        value: this.state.title
+      })
+    ]);
   }
 });
 
@@ -238,19 +258,16 @@ var DateSelect = component('DateSelect', {
     dateChange(event.target.value);
   },
   render: function() {
-    return div(
-      null,
-      [
-        React.DOM.label(null, 'Date'),
-        React.DOM.select(
-          {onChange: this.handleChange},
-          ['Introductory Clause', 'Each Signature']
-            .map(function(value) {
-              return React.DOM.option({value: value}, value);
-            })
-        )
-      ]
-    );
+    return div(null, [
+      React.DOM.label(null, 'Date'),
+      React.DOM.select(
+        {onChange: this.handleChange},
+        ['Introductory Clause', 'Each Signature']
+          .map(function(value) {
+            return React.DOM.option({value: value}, value);
+          })
+      )
+    ]);
   }
 });
 
@@ -259,47 +276,40 @@ var SettingsForm = component('SettingsForm', {
     event.preventDefault();
   },
   render: function() {
-    return React.DOM.form(
-      {onSubmit: this.handleSubmit},
-      [
-        create(TitleInput, {title: this.props.title}),
-        create(DateSelect, null)
-      ]
-    );
+    return React.DOM.form({onSubmit: this.handleSubmit}, [
+      create(TitleInput, {title: this.props.title}),
+      create(DateSelect)
+    ]);
   }
 });
 
 var Paragraph = render('Paragraph', function() {
   if (this.props.date === 'Introductory Clause') {
-    return p(
-      null,
+    return p(null, [
       'The parties are signing this ' + this.props.title +
       ' on the date stated in the introductory clause.'
-    );
+    ]);
   } else {
-    return p(
-      null,
+    return p(null, [
       'The parties are signing this ' + this.props.title +
       ' on the dates written beside their respective signatures.'
-    );
+    ]);
   }
 });
 
 var Page = render('Page', function() {
-  return div(
-    {className: 'page'},
-    [
-      create(DeleteButton, {index: this.props.index}),
-      create(Paragraph, {
-        date: this.props.date,
-        title: this.props.title
-      }),
-      create(Block, {
-        date: this.props.date,
-        party: this.props.party
-      })
-    ]
-  );
+  return div({className: 'page'}, [
+    create(DeleteButton, {index: this.props.index}),
+    create(Paragraph, {
+      date: this.props.date,
+      title: this.props.title
+    }),
+    create(Block, {
+      index: this.props.index,
+      date: this.props.date,
+      party: this.props.party
+    })
+  ]);
 });
 
 function button(name, text, handleClick) {
@@ -315,7 +325,7 @@ var AddButton = button('AddButton', 'Add', function() {
   addBlock();
 });
 
-var DeleteButton = button('DeleteButton', 'Delete', function() {
+var DeleteButton = button('DeleteButton', 'Delete Page', function() {
   deleteBlock(this.props.index);
 });
 
@@ -350,9 +360,9 @@ var Project = component('Project', {
             title: project.get('title')
           });
         }),
-      create(AddButton, null),
+      create(AddButton),
       ' ',
-      create(PrintButton, null)
+      create(PrintButton)
     ]);
   }
 });
